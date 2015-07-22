ba7038c7348133a203e3c7b03248cbb7e2cf8a27
Kristin Beltz <beltz08@me.com>
2015-07-22 14:30:16 -0500

    Add routes for index and show. Add show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
+  def show
+    @user = User.find(params[:id])
+    # Automatically load the view in /views/users/show.html.erb
+  end
 end
